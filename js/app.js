47c50840967449377d9e850206654813405e1b28
Joshua Frazer <joshua.frazer.school@gmail.com>
2022-06-14 13:38:23 -0700

    refactored to use constructor; reformat display to table

--- js/app.js
+++ js/app.js
@@ -1,118 +1,191 @@
 'use strict';
 
-let store = {
-  //after constructor function is added these can all be added to prototype
-  numberCustomersRand: function(){
-    let customers = Math.round(Math.random() * (this.hourlyCust.max - this.hourlyCust.min) + this.hourlyCust.min);
-    return customers;
-  },
-  addHourlySale: function(){
-    let sales = Math.round(this.numberCustomersRand() * this.avgSale);
-    this.totalSales += sales;
-    this.hourlySales.push(sales);
-  },
-  displaySales: function(){
-    let time = 6;
-    const body = document.getElementById('salesBody');
-    const section = document.createElement('section');
-    const list = document.createElement('ul');
-    for (let item of this.hourlySales){
-      let listItem = document.createElement('li');
-      listItem.appendChild(document.createTextNode(`${this.timeConvert(time)}: ${item}`));
-      listItem.classList.add('data');
-      list.appendChild(listItem);
-      time++;
-    }
-    let listItem = document.createElement('li');
-    listItem.appendChild(document.createTextNode(`Total: ${this.totalSales} cookies`));
-    list.appendChild(listItem);
-
-    section.appendChild(document.createTextNode(`${this.storeName}\n`));
-    section.appendChild(list);
-    section.classList.add('highlight');
-    body.appendChild(section);
-  },
-  timeConvert: function(milTime){
+function createDailySalesObject(startTime, endTime){
+  let timeList = {};
+  for(let milTime = startTime; milTime < endTime; milTime++){
+    let time;
     if (milTime < 12){
-      return `${milTime}am`;
+      time = `${milTime}am`;
     }
     else if (milTime === 12){
-      return `${milTime}pm`;
+      time = `${milTime}pm`;
     }
     else {
-      return `${milTime - 12}pm`;
+      time = `${milTime - 12}pm`;
     }
-  },
-  addDaysSales: function(hours) {
-    for (let i = 0; i < hours; i++){
-      this.addHourlySale();
-    }
+    timeList[time] = 0;
+  }
+  return timeList;
+}
+
+function Store(storeName, minHourlyCusts, maxHourlyCusts, avgSalesPerCust, startTime, endTime){
+  this.storeName = storeName;
+  this.hourlyCusts = {min: minHourlyCusts, max: maxHourlyCusts};
+  this.avgSale = avgSalesPerCust;
+  this.openTime = startTime;
+  this.closeTime = endTime;
+  this.dailySales = createDailySalesObject(this.openTime, this.closeTime);
+}
+
+Store.prototype.randomHourlySales = function(){
+  let customers = Math.round(Math.random() * (this.hourlyCusts.max - this.hourlyCusts.min) + this.hourlyCusts.min);
+  return Math.round(customers * this.avgSale);
+};
+
+Store.prototype.addDaySales = function(){
+  for(let hour in this.dailySales){
+    this.dailySales[hour] = this.randomHourlySales();
   }
 };
 
-function addProperties(subObj, superObj){
-  //this func will be replaced by adding to prototype after constructor is created
-  for (let key in superObj){
-    subObj[key] = superObj[key];
+Store.prototype.totalSales = function(){
+  let total = 0;
+  for(let hour in this.dailySales){
+    total += this.dailySales[hour];
   }
+  return total;
+};
+
+Store.prototype.render = function(table){
+  let row = document.createElement('tr');
+
+  //add name
+  let cell = document.createElement('td');
+  cell.classList.add('highlight');
+  cell.textContent = this.storeName;
+  row.appendChild(cell);
+
+  //add cell to row for each hour of sales, but enter 0 for hours this store is not open
+  let headerRow = table.rows[0].cells;
+  let times = [];
+  for(let i = 1; i < headerRow.length - 1; i++){
+    times.push(headerRow[i].textContent);
+  }
+  for(let time of times){
+    cell = document.createElement('td');
+    cell.classList.add('data');
+    if (this.dailySales[time]){
+      cell.textContent = this.dailySales[time];
+    }
+    else {
+      cell.textContent = 0;
+    }
+    row.appendChild(cell);
+  }
+
+  //add daily total
+  cell = document.createElement('td');
+  cell.classList.add('data');
+  cell.textContent = this.totalSales();
+  row.appendChild(cell);
+
+  table.appendChild(row);
+};
+
+//create an object with store names as keys and hourly sales as an array of numbers
+Store.tableDataObjectCreate = function(table){
+  let data = {};
+  
+  //get all rows from table excluding first row which is headers
+  for(let i = 1; i < table.rows.length; i++){
+    let row = table.rows[i];
+    //add an array to new object with the name as key
+    data[row.cells[0].textContent] = []; 
+    //get all cells from row, excluding first cell which is name
+    for(let j = 1; j < row.cells.length; j++){
+      let cell = row.cells[j];
+      data[row.cells[0].textContent].push(parseInt(cell.textContent));
+    }
+  }
+  return data;
+};
+
+Store.buildHeaderRow = function(startTime, endTime){
+  let row = document.createElement('tr');
+  let header = document.createElement('th');
+  header.textContent = 'Store Name';
+  row.appendChild(header);
+
+  let hoursObject = createDailySalesObject(startTime, endTime);
+  for(let hour in hoursObject){
+    header = document.createElement('th');
+    header.textContent = hour;
+    row.appendChild(header);
+  }
+
+  header = document.createElement('th');
+  header.textContent = 'Daily Location Total';
+  row.appendChild(header);
+  return row;
+};
+
+Store.buildFooterRow = function(table){
+  let row = document.createElement('tr');
+  let cell = document.createElement('th');
+  cell.textContent = 'Totals';
+  row.appendChild(cell);
+
+  let data = Store.tableDataObjectCreate(table);
+  //get first key so can see array length
+  let firstStore = Object.keys(data)[0];
+  for(let columnNumber = 0; columnNumber < data[firstStore].length; columnNumber++){
+    let columnTotal = 0;
+    for(let storeName in data){
+      columnTotal += data[storeName][columnNumber];
+    }
+    let cell = document.createElement('th');
+    cell.textContent = columnTotal;
+    cell.classList.add('data');
+    row.appendChild(cell);
+  }
+  return row;
+};
+
+Store.buildTable = function(storesList){
+  //create header based on earliest and latest times needed to show; in case not all stores are same hours
+  let minStart = 24;
+  let maxEnd = 0;
+  for(let store of storesList){
+    if(store.openTime < minStart){
+      minStart = store.openTime;
+    }
+    if(store.closeTime > maxEnd){
+      maxEnd = store.closeTime;
+    }
+  }
+  let header = Store.buildHeaderRow(minStart, maxEnd);
+
+  let table = document.createElement('table');
+  table.appendChild(header);
+
+  //add all stores to table
+  for(let store of storesList){
+    store.render(table);
+  }
+  //add footer
+  table.appendChild(Store.buildFooterRow(table));
+  return table;
+};
+
+//MAIN: Use the objects and their methods
+
+let storesList =
+[
+  new Store('Seattle', 23, 65, 6.3, 6, 20),
+  new Store('Tokyo', 3,24, 1.2, 6, 20),
+  new Store('Dubai', 23, 65, 6.3, 6, 20),
+  new Store('Paris', 20, 38, 2.3, 6, 20),
+  new Store('Lima', 2, 16, 4.6, 6, 20)
+];
+
+for (let store of storesList){
+  store.addDaySales();
 }
 
-function runDailySales(aStore){
-  //after constructor created and prototypes added, this can be replaced with a method
-  addProperties(aStore, store);
-  aStore.addDaysSales(14);
-  aStore.displaySales();
-}
+let table = Store.buildTable(storesList);
 
-let seattle = {
-  storeName: 'Seattle',
-  hourlyCust: {min: 23, max: 65},
-  avgSale: 6.3,
-  hourlySales: [],
-  totalSales: 0
-};
-runDailySales(seattle);
-
-let tokyo = {
-  storeName: 'Tokyo',
-  hourlyCust: {min: 3, max: 24},
-  avgSale: 1.2,
-  hourlySales: [],
-  totalSales: 0
-};
-runDailySales(tokyo);
-
-let dubai = {
-  storeName: 'Dubai',
-  hourlyCust: {min: 23, max: 65},
-  avgSale: 6.3,
-  hourlySales: [],
-  totalSales: 0
-};
-runDailySales(dubai);
-
-let paris = {
-  storeName: 'Paris',
-  hourlyCust: {min: 20, max: 38},
-  avgSale: 2.3,
-  hourlySales: [],
-  totalSales: 0
-};
-runDailySales(paris);
-
-let lima = {
-  storeName: 'Lima',
-  hourlyCust: {min: 2, max: 16},
-  avgSale: 4.6,
-  hourlySales: [],
-  totalSales: 0
-};
-runDailySales(lima);
-
-
-
-
-
+let body = document.getElementById('salesBody');
+body.appendChild(table);
 
 
 
